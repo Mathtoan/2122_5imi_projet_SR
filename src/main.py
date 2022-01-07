08e74ae8d0dc07d33d4625afcc99efe57d15aee2
Mathieu Duong <mathieu.duong1@gmail.com>
2022-01-07 11:21:15 +0100

    gaussian filter on fourier domain + saving HR grid
    Co-authored-by: celialecpe <celialecpe@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+#%% Import
 import argparse
 import os
 
+from numpy import float64
+
 from utils import *
 
-# Parser
+#%% Parser
 parser = argparse.ArgumentParser(description='Super Resolution')
 parser.add_argument('-d', '--device', type=str, default='iPhone13Pro',
                     help='Choose the device', choices=['iPhone13Pro'])
@@ -13,24 +16,28 @@ parser.add_argument('-f', '--upscale_factor', type=int, default='4',
                     help='Choose the upscaling factor')
 parser.add_argument('-i', '--iterations', type=int, default='100',
                     help='Choose the number of iterations')
-parser.add_argument('-o', '--sigma', type=float, default='0.4',
+parser.add_argument('-o', '--sigma', type=float, default='0.05',
                     help='Choose the value of sigma')
 parser.add_argument('-c', '--color', type=str, default='gray',
                     help='Choose the color of the output image', choices=['gray','rgb'])
+parser.add_argument('-r', '--ref', type=int, default='9',
+                    help='Choose the reference image')
 
 args = parser.parse_args()
 
-# Parameters
+#%% Parameters
 upscale_factor = args.upscale_factor
 it = args.iterations
 sigma = args.sigma
 color = args.color
+idx_ref = args.ref
 
 print('RUNNING PARAMETER', 
       '\nUpscale factor :', upscale_factor,
-      '\nNumber of iteration of Papoulis-Gerchberg :', it)
+      '\nNumber of iteration of Papoulis-Gerchberg :', it,
+      '\nSigma :', sigma)
 
-# Path
+#%% Path
 input_dir = os.path.join('fig', args.device, args.scene)
 output_dir = os.path.join('output', args.device, args.scene, 'up_'+str(upscale_factor)+'_it_'+str(it)+'_sigma_'+str(sigma))
 
@@ -38,24 +45,36 @@ list_image_input_dir = [os.path.join(input_dir, i) for i in os.listdir(input_dir
 list_image_input_dir.sort()
 
 
-# Registration and SR grid creation
+#%% Registration and SR grid creation
 # for idx_ref in range(len(list_image_input_dir)):
 #     im_ref, im_to_register_list, registration_shifts = computing_regitration(list_image_input_dir, idx_ref, upscale_factor)
 
-idx_ref = 9
-im_groundtruth = rgb2gray(io.imread(list_image_input_dir[idx_ref]))
-im_ref, im_to_register_list, registration_shifts = computing_regitration(list_image_input_dir, idx_ref, upscale_factor)
+im_groundtruth = io.imread(list_image_input_dir[idx_ref])
+if color=='gray':
+    im_groundtruth = rgb2gray(im_groundtruth)
 
-HR_grid = creation_HR_grid(im_ref, upscale_factor, im_to_register_list, registration_shifts, color)
+HR_grid_txt_dir = os.path.join('output', args.device, args.scene, 'HR_grid_'+str(idx_ref)+'.txt')
+# Load saved HR grid if already generated
+if os.path.exists(HR_grid_txt_dir):
+    print('Loading ', HR_grid_txt_dir)
+    HR_grid = np.loadtxt(HR_grid_txt_dir, dtype=float)
+    im_ref = rescale(rgb2gray(io.imread(list_image_input_dir[idx_ref])), 1/upscale_factor)
+    print(HR_grid.shape)
+else:
+    im_ref, im_to_register_list, registration_shifts = computing_regitration(list_image_input_dir, idx_ref, upscale_factor)
+    HR_grid = creation_HR_grid(im_ref, upscale_factor, im_to_register_list, registration_shifts, color)
+    np.savetxt(HR_grid_txt_dir, HR_grid, fmt='%f')
 
 if not(os.path.exists(output_dir)):
     os.makedirs(output_dir)
+io.imsave(os.path.join(output_dir,'hr_grid_'+str(idx_ref)+'.png'), HR_grid)
+
 io.imsave(os.path.join(output_dir,'groundtruth.png'), im_groundtruth)
 io.imsave(os.path.join(output_dir,'lr_image.png'), im_ref)
-io.imsave(os.path.join(output_dir,'hr_grid.png'), HR_grid)
 
+#%% Papoulis-Gerchberg method
 im_sr = PG_method(HR_grid, im_ref, sigma, upscale_factor, it)
-io.imsave(os.path.join(output_dir,'sr_image.png'), im_sr)
+io.imsave(os.path.join(output_dir,'sr_image_new.png'), im_sr.real)
 
 if color=='gray':
     colmap='gray'
@@ -76,7 +95,7 @@ plt.subplot(223)
 plt.imshow(HR_grid, colmap)
 plt.title('HR grid')
 plt.subplot(224)
-plt.imshow(im_sr, colmap)
+plt.imshow(im_sr.real, colmap)
 plt.title('SR image')
 plt.show()
 plt.close()
